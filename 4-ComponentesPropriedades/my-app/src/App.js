8901162c0466212c7ab2ce58f4ced7682118cd91
hudney-fsbrito <hudney.rse@gmail.com>
2023-05-11 14:21:12 -0300

    Componentes e Prorpiendades

---
+++ 4-ComponentesPropriedades/my-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from './Header'
+import './App.css';
+
+function App() {
+  return (
+    <Header name="Hudney" links={["Sobre","Comprar","Contato"]}></Header>
+  );
+}
+
+export default App;
